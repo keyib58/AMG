d307cbdc0d6036973eb2c0c01fe1f6d584a9a5a2
KeYi <46948667+bean4896@users.noreply.github.com>
2024-08-06 10:24:36 +0800

    debug

--- components/news/newsListing.tsx
+++ components/news/newsListing.tsx
@@ -1,6 +1,6 @@
 import { getSortedNewsData } from '@/lib/news';
 import { NewsType } from 'types/newsType';
-import NewsCard from './newsCard';
+import CardNews from './cardNews';
 
 export default function NewsListing() {
   const allNewsData: NewsType[] = getSortedNewsData();
@@ -8,7 +8,7 @@ export default function NewsListing() {
   return (
     <ul className="w-full grid grid-cols-1 md:grid-cols-4 gap-6">
       {allNewsData.map((news) => (
-        <NewsCard key={news.slug} news={news} />
+        <CardNews key={news.slug} news={news} />
       ))}
     </ul>
   );
