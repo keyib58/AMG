3ffcb00eb9e6f28113d0c5bd07f4f0dab1a98f71
KeYi <46948667+bean4896@users.noreply.github.com>
2024-08-07 18:08:30 +0800

    RENAME FILE DEPLOY

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-import Card from "@/components/home/card";
-import { DEPLOY_URL } from "@/lib/constants";
-import { Github, Twitter } from "@/components/shared/icons";
-import WebVitals from "@/components/home/web-vitals";
-import ComponentGrid from "@/components/home/component-grid";
-import Image from "next/image";
-import { nFormatter } from "@/lib/utils";
 import HomeContainer from "@/components/home/HomeContainer";
 
 export default async function Home() {
