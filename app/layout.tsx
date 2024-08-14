274a016bc2b2753b8a21902fab97b1f60ece7b7d
KeYi <46948667+bean4896@users.noreply.github.com>
2024-08-14 14:05:20 +0800

    debug meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-
 import "./globals.css";
 import cx from "classnames";
 import { sfPro, inter } from "./fonts";
@@ -9,15 +8,34 @@ import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
 import SessionWrapper from "@/components/SessionWrapper";
 import ReduxProvider from "@/components/ReduxProvider";
 import SessionLogger from "@/components/shared/SessionLogger";
+import Head from "next/head"; // Import Head
 
 export const metadata = {
   title: "Kingmidas Games",
-  description: "",
+  description: "Join us to explore amazing opportunities and claim your goodie bag!",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const imageUrl = '/socialShare.jpg'; // Path to the image in the public folder
+  const shareUrl = 'https://km2024-jul.vercel.app/'; // Replace with your actual URL
+
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:url" content={shareUrl} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Kingmidas Games" />
+        <meta property="og:description" content="Join us to explore amazing opportunities and claim your goodie bag!" />
+        <meta property="og:image" content={imageUrl} />
+        <meta property="og:image:width" content="1600" />
+        <meta property="og:image:height" content="900" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Kingmidas Games" />
+        <meta name="twitter:description" content="Join us to explore amazing opportunities and claim your goodie bag!" />
+        <meta name="twitter:image" content={imageUrl} />
+        <meta name="twitter:url" content={shareUrl} />
+      </Head>
       <body className={cx(sfPro.variable, inter.variable, "bg-[#0d0d0d]")}>
         <ReduxProvider>
           <SessionWrapper>
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,11 @@
-import Head from 'next/head';
 import HomeContainer from "@/components/home/HomeContainer";
 
 export default async function Home() {
-  const title = 'Welcome to KingMidas!';
-  const description = 'Join us to explore amazing opportunities and claim your goodie bag!';
-  const shareUrl = 'https://km2024-jul.vercel.app/'; // Replace with your actual URL
-  const imageUrl = '/socialShare.jpg'; // Path to the image in the public folder
 
   return (
     <>
-      <Head>
-        <title>{title}</title>
-        {/* Open Graph Meta Tags */}
-        <meta property="og:url" content={shareUrl} />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={imageUrl} />
-        <meta property="og:image:width" content="1600" />
-        <meta property="og:image:height" content="900" />
-
-        {/* Twitter Meta Tags */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={imageUrl} />
-        <meta name="twitter:url" content={shareUrl} />
-      </Head>
       <HomeContainer />
     </>
   );
 }
+
