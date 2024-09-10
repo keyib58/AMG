5e2a8ea59d8efd645861dfe53242447e2fc3163f
KeYi <46948667+bean4896@users.noreply.github.com>
2024-09-10 14:58:55 +0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import HomeContainer from "@/components/home/HomeContainer";
 
-export default async function Home() {
 
-  return (
+export default async function Home() {
+console.log("This is UK website");
+return (
     <>
       <HomeContainer />
     </>
