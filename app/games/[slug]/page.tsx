a06880233fa4e65a7e184cb74c8b42541343301b
KeYi <keyi.dai@dynamitegames.io>
2024-10-23 16:31:12 +0800

    page optimize

--- app/games/[slug]/page.tsx
+++ app/games/[slug]/page.tsx
@@ -1,25 +1,26 @@
 // app/games/[slug]/page.tsx
 
-import { PrismaClient } from '@prisma/client';
-import GameInfo from '@/components/game_detail/gameInfo';
-import { Game } from 'types/type';
+// import { PrismaClient } from '@prisma/client';
+// import GameInfo from '@/components/game_detail/gameInfo';
+// import { Game } from 'types/type';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 export default async function GamePage({ params }: { params: { slug: string } }) {
-  const game = await prisma.game.findUnique({
-    where: { slug: params.slug },
-    include: {
-      tags: true,
-      languageInfo: true,
-    },
-  });
+  // const game = await prisma.game.findUnique({
+  //   where: { slug: params.slug },
+  //   include: {
+  //     tags: true,
+  //     languageInfo: true,
+  //   },
+  // });
 
-  if (!game) {
-    return <div>Game not found</div>;
-  }
+  // if (!game) {
+  //   return <div>Game not found</div>;
+  // }
 
   return (
-      <GameInfo game={game as Game} placeHolderUrl={''} />
+      // <GameInfo game={game as Game} placeHolderUrl={''} />
+      <></>
   );
 }
