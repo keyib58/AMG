d529f8051bed6ff3c384757f2a00db2512ca9c9f
KeYi <keyi.dai@dynamitegames.io>
2024-10-23 15:38:59 +0800

    optimize  api

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -1,31 +1,31 @@
-// // app/api/submit-form/route.ts
+// app/api/submit-form/route.ts
 
-// import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-// export async function PUT(request: NextRequest) {
-//   const { email } = await request.json();
+export async function PUT(request: NextRequest) {
+  const { email } = await request.json();
 
-//   const response = await fetch(
-//     `https://api.hsforms.com/submissions/v3/integration/submit/40119618/7d6a6b52-8b3a-4667-8ff7-7bddbabe4d49`,
-//     {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({
-//         fields: [
-//           {
-//             name: 'email',
-//             value: email,
-//           },
-//         ],
-//       }),
-//     }
-//   );
+  const response = await fetch(
+    `https://api.hsforms.com/submissions/v3/integration/submit/40119618/7d6a6b52-8b3a-4667-8ff7-7bddbabe4d49`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        fields: [
+          {
+            name: 'email',
+            value: email,
+          },
+        ],
+      }),
+    }
+  );
 
-//   if (response.ok) {
-//     return NextResponse.json({ message: 'Form submitted successfully' });
-//   } else {
-//     return NextResponse.json({ message: 'Failed to submit form' }, { status: 500 });
-//   }
-// }
+  if (response.ok) {
+    return NextResponse.json({ message: 'Form submitted successfully' });
+  } else {
+    return NextResponse.json({ message: 'Failed to submit form' }, { status: 500 });
+  }
+}
