6b57f7483ef48cc91719aec061bf089701eb7a53
KeYi <keyi.dai@dynamitegames.io>
2024-10-24 15:25:01 +0800

    edit route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,12 @@
 // import NextAuth from "next-auth";
 // import { authOptions } from "./options";
 
+import { NextRequest } from "next/server";
+
 // const handler = NextAuth(authOptions);
 
 // export { handler as GET, handler as POST };
 // app/api/upload/route.ts
+export async function GET(request: NextRequest) {
+    return new Response('Hello World');
+  }
