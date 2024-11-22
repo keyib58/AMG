4f3fa89ac853a9545811cb886f0a75605acb9d12
bean4896 <dkybone1996@gmail.com>
2024-11-22 11:26:06 +0800

    api contact

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -2,10 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 import sendgrid from "@sendgrid/mail";
 
 // Set your SendGrid API key
-if (!process.env.SENDGRID_API_KEY) {
+if (process.env.SENDGRID_API_KEY) {
+  sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+} else {
   throw new Error("SENDGRID_API_KEY is not defined");
 }
-sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
 export async function POST(request: NextRequest) {
   const { firstname, lastname, email, phone, department, subject, message } = await request.json();
@@ -36,7 +37,10 @@ export async function POST(request: NextRequest) {
 
     return NextResponse.json({ message: "Form submitted successfully and email sent!" });
   } catch (error) {
-    console.error("Error sending email:", error.response?.body || error.message);
+    // Ensure error is cast as any to safely access its properties
+    const err = error as any;
+    console.error("Error sending email:", err?.response?.body || err?.message || error);
+
     return NextResponse.json(
       { message: "Failed to send email. Please try again later." },
       { status: 500 }
